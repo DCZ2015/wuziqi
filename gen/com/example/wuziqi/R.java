f63146e117fe7cb95c338a33da872d9c40ab4504
DXX <573608754@qq.com>
2015-11-08 00:13:31 +0800

    双人对战

--- gen/com/example/wuziqi/R.java
+++ gen/com/example/wuziqi/R.java
@@ -21,10 +21,17 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int back=0x7f020000;
+        public static final int black=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int restart=0x7f020003;
+        public static final int white=0x7f020004;
+        public static final int wood_background=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int buttonb=0x7f080001;
+        public static final int buttonw=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/wuziqi/MainActivity.java
+++ src/com/example/wuziqi/MainActivity.java
@@ -5,18 +5,48 @@ package com.example.wuziqi;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 	
-	Chessboard chessboard;
+	Button buttonb;
+    Button buttonw;
+    ChessBoard chessboard;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		chessboard = new Chessboard(this);
-		setContentView(chessboard);
+		buttonb = (Button) findViewById(R.id.buttonb);
+		buttonw = (Button) findViewById(R.id.buttonw);
+		ClickButton clickbutton=new ClickButton();
+		buttonb.setOnClickListener(clickbutton);
+		buttonw.setOnClickListener(clickbutton);
+  		chessboard = new ChessBoard(this);
+        chessboard.setBackgroundResource(R.drawable.wood_background);
 	}
+	
+	class ClickButton implements OnClickListener{
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			 switch (v.getId()) {  
+		            case R.id.buttonb:{  
+		            	chessboard.playerStatus=0;
+		            	setContentView(chessboard);
+		            }break;  
+		            case R.id.buttonw:{  
+		            	chessboard.playerStatus=1;  
+		            	setContentView(chessboard);
+		            }  break;    
+		            default:  
+		                break;  
+		        }  
+		}
+
+     }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
